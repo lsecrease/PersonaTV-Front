2cf144b47a081033e22daa498739cc0ec2350d72
Diego Durli <dgodurli@gmail.com>
2016-04-28 17:30:40 -0300

    Refactor render call.

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import Store from './store';
 import App from './containers/App';
 
-ReactDOM.render(
+render(
     <Store>
         <App />
     </Store>,
