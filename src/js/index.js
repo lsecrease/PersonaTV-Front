6b9ef3b4b1b7584f3607e276277830f8ee7f362d
Diego Durli <dgodurli@gmail.com>
2016-04-28 18:36:14 -0300

    Render directly a React-Redux Provider and its routes.

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Store from './store';
+import { Provider } from 'react-redux';
+import { Router } from 'react-router';
+import { store, routes, history } from './store';
 
 // Application Main CSS
 require('../styles/main.scss');
 
 render(
-    <Store />,
+    <Provider store={store}>
+        <Router history={history} routes={routes} />
+    </Provider>,
     document.getElementById('app')
 );
