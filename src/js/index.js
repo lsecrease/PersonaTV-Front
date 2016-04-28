49a8223ed0d8815b179b37378812486ac87efad2
Diego Durli <dgodurli@gmail.com>
2016-04-28 17:50:57 -0300

    Add styles import into app index.

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { render } from 'react-dom';
 import Store from './store';
 
+// Application Main CSS
+require('../styles/main.scss');
+
 render(
     <Store />,
     document.getElementById('app')
