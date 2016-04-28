3aecde983e4c84993b410189d5791b94dad4a330
Diego Durli <dgodurli@gmail.com>
2016-04-28 17:40:01 -0300

    Avoid rendering any App component here. Will be handled by Router.

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Store from './store';
-import App from './containers/App';
 
 render(
-    <Store>
-        <App />
-    </Store>,
+    <Store />,
     document.getElementById('app')
 );
