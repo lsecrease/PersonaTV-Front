c11fad13081296bf4da83ba06876a5c20c60fb81
Diego Durli <dgodurli@gmail.com>
2016-05-05 16:30:13 -0300

    Add Login Modal raw UI.

--- src/js/components/Login.js
+++ src/js/components/Login.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { push } from 'react-router-redux';
+import { Dialog, RaisedButton } from 'material-ui';
+import { Facebook, Twitter } from './icons';
 
 export default class Login extends Component {
 
@@ -12,14 +14,53 @@ export default class Login extends Component {
     }
 
     render() {
-        const { setUserAuthenticated } = this.props.actions;
-
         return (
-            <div>
-                <button onClick={setUserAuthenticated.bind(this, true)}>
-                    Log me in
-                </button>
-            </div>
+            <Dialog
+                title='Ready to Join?'
+                modal={false}
+                open={true}
+                contentStyle={{width: '25%'}}
+                titleStyle={{fontSize: '40px', fontWeight: '100'}}>
+
+                <div className='login_passport_buttons' style={{
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    marginTop: '2em'}}>
+
+                    <RaisedButton
+                        label="Login with Facebook"
+                        linkButton={false}
+                        secondary={true}
+                        backgroundColor={'#3A579A'}
+                        icon={<Facebook styles={{width: '11px', height: '21px', fill: '#FFF'}} viewBox='0 0 11 21' />} />
+
+                    <RaisedButton
+                        label="Login with Twitter"
+                        linkButton={false}
+                        secondary={true}
+                        backgroundColor={'#3C97FF'}
+                        icon={<Twitter styles={{width: '21px', height: '16px', fill: '#FFF'}} viewBox='0 0 21 16' />} />
+                </div>
+
+                <div className='signin_email' style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    marginTop: '1em'}}>
+
+                    <p style={{color: '#030303'}}>
+                        Or Sign In via Email
+                    </p>
+
+                    <p style={{fontSize: '10px', color: 'gray'}}>
+                        By signing in, you agree to PersonaTV's Terms and Conditions of Use and Privacy Policy
+                    </p>
+
+                    <a href='#' style={{fontSize: '12px', color: '#030303', marginTop: '0.5em'}}>
+                        No Thanks, I don't want to create an account
+                    </a>
+                </div>
+            </Dialog>
         );
     }
 }
