85ac1bca96a920862f7e4b3575b3a391a6bd5437
Diego Durli <dgodurli@gmail.com>
2016-05-05 16:30:33 -0300

    Import React Tap Event Plugin for Material UI usage.

--- src/js/index.js
+++ src/js/index.js
@@ -3,9 +3,16 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router';
 import { store, routes, history } from './store';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
 // Application Main CSS
 require('../styles/main.scss');
+
+// Needed for onTouchTap
+// Can go away when react 1.0 release
+// Check this repo:
+// https://github.com/zilverline/react-tap-event-plugin
+injectTapEventPlugin();
 
 render(
     <Provider store={store}>
