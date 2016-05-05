c35486f6fdfcc96c13c0e4e31380447dde95ba00
Diego Durli <dgodurli@gmail.com>
2016-05-05 18:39:55 -0300

    Add routes for explore and favorites.

--- src/js/store/routes.js
+++ src/js/store/routes.js
@@ -8,15 +8,17 @@ import Login from '../containers/Login';
 import Home from '../components/Home';
 import LiveStreaming from '../components/LiveStreaming';
 import NotFound from '../components/NotFound';
-import Channels from '../components/Channels';
+import Explore from '../components/Explore';
+import Favorites from '../components/Favorites';
 import AuthenticatedRoute from '../components/AuthenticatedRoute';
 
 export const routes = (
     <div>
         <Route path='/' component={Home}>
             <Route path='login' component={Login} />
-            <Route path='live' component={AuthenticatedRoute(LiveStreaming)} />
-            <Route path='channels' component={AuthenticatedRoute(Channels)} />
+            <Route path='live' component={LiveStreaming} />
+            <Route path='explore' component={Explore} />
+            <Route path='favorites' component={AuthenticatedRoute(Favorites)} />
         </Route>
         <Route path='*' component={NotFound} />
     </div>
