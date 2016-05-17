862de445838d6a0e9d8714fcf628a0620b33218f
Diego Durli <dgodurli@gmail.com>
2016-05-16 21:08:26 -0300

    Export Youtube icon.

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,9 +1,11 @@
 import { Facebook } from './Facebook';
 import { Twitter } from './Twitter';
 import { Notifications } from './Notifications';
+import { Youtube } from './Youtube';
 
 export {
     Facebook,
     Twitter,
-    Notifications
+    Notifications,
+    Youtube
 };
